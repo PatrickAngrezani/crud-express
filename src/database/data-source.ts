a2692d880daf0cb9c08408b4bb68aebac3ae6f9b
PatrickAngrezani <pangrezani@gmail.com>
2025-09-17 16:53:05 -0300

    feat: configure db

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -5,9 +5,11 @@ import { User } from "../entities/User";
 import dotenv from "dotenv";
 dotenv.config();
 
+const dbHost = process.env.NODE_ENV === 'development' ? 'localhost' : 'db'; 
+
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST || "localhost",
+  host: dbHost,
   port: Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USERNAME,
   password: String(process.env.DB_PASSWORD),
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import dotenv from "dotenv";
 import app from "./app";
 import { AppDataSource } from "./database/data-source";
+import dotenv from "dotenv";
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
